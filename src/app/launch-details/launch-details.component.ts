136ceff1670d505dce82dfb844ceb73a2ff2605c
thiagoMoraes21 <thiago4x70@gmail.com>
2020-07-06 08:59:32 -0300

    implemented launch details page

--- src/app/launch-details/launch-details.component.ts
+++ src/app/launch-details/launch-details.component.ts
@@ -1,16 +1,30 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { LauchDetailsGQL } from '../services/spacexGraphql.service'
+import { map, switchMap } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-launch-details',
-  templateUrl: './launch-details.component.html',
-  styleUrls: ['./launch-details.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+	selector: 'app-launch-details',
+	templateUrl: './launch-details.component.html',
+	styleUrls: ['./launch-details.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LaunchDetailsComponent implements OnInit {
 
-  constructor() { }
+	constructor(
+		private route: ActivatedRoute,
+		private launchDetailsService: LauchDetailsGQL
+	) { }
 
-  ngOnInit(): void {
-  }
+	launchDetails$ = this.route.paramMap.pipe(
+		switchMap(params => {
+			const id = params.get('id');
+			return this.launchDetailsService.fetch({ id });
+		}),
+		map(res => res.data.launch)
+	);
+
+	ngOnInit(): void {
+	}
 
 }
